8509aa678dd9d39140747fc7fac5e4c86b4d505c
NeelBit <nelbertran@gmail.com>
2022-10-11 23:06:08 -0300

    Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+    title = 'Registro de usuarios';
+
+    mensajeProperty = "";
+
+    registradoProperty = false;
+
+    nombreProp: String = "";
+    emailProp: String = "";
+    cargoProp: String = "";
+    passwordProp: String = "";
+
+    listaU: String[]= ["uno", "dos", "tres"];
+
+    /* entradas: object[]; */
+    entradas: Array<{tituloEntrada: String}>;
+
+    constructor() {
+        this.entradas = [
+            {tituloEntrada: "esto es el titulo numero 1"},
+            {tituloEntrada: "segundo texto2!!! segundo titulo"},
+            {tituloEntrada: "Tercer titulo de entrada de blog"},
+            {tituloEntrada: "Cuarto titulo 4º, cuarta entrada de blog title"},
+        ]
+    }
+
+    registrarse(name: String, email: String, pass: String) {
+
+        if (name.length >= 4) {
+            this.registradoProperty = true;
+        }
+        
+        this.mensajeProperty = `Usuario registrado exitosamente: ${name}, ${email} y su contraseña es: ${pass}`;
+
+
+    }
+
 }
